a1c2c77b2e07641a5fe062bc127256da817965d7
Papri <papri.sen2020@gmail.com>
2025-09-28 13:16:29 +0530

    Create Exp-17 Linked list in CPP_1.cpp

---
+++ Exp-17 Linked list in CPP_1.cpp
@@ -0,0 +1,29 @@
+// Papri Sen
+// 24070123070
+// A3
+
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+
+    int val;
+    Node* next;
+
+    Node(int data){
+        val = data;
+        next = NULL;
+    }
+};
+
+int main(){
+    Node* n=new Node(20);
+    cout<<n->val<<" "<<n->next<<endl;
+    return 0;
+}
+
+/*
+OUTPUTS:
+20 0
+*/
